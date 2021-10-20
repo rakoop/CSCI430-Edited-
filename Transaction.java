64f5645014001c56544b20b321f21ed611494d44
rakoop <57457046+rakoop@users.noreply.github.com>
2021-10-19 21:40:11 -0500

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,1 +1,33 @@
+public class Transaction extends Thing {
+	private static final long serialVersionUID = 1530053204090450305L;
+	private int transactionID;
+	private int clientID;
+	private double balance;
+
+	public Transaction(int tID, int cID, double bal) {
+		this.transactionID = tID;
+		this.clientID = cID;
+		this.balance = bal;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public int getClientID() {
+		return clientID;
+	}
+
+	public void setBalance(double amount) {
+		this.balance = amount;
+	}
+
+	public int getID() {
+		return transactionID;
+	}
+
+	public Transaction instance() {
+		return this;
+	}
+}
 
